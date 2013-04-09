4f25acef1c3d1c84e12ebfafb9aa80e5a9a96983
moza <moza@lillypulitzer.com>
2013-04-09 11:29:19 -0400

    Date
    If the request date is less than 1 week from the day the request has
    been made, show an error message linking to Kathleen's email. This was
    not ht greatest error mesasge.

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,6 +1,16 @@
 class Request < ActiveRecord::Base
   attr_accessible :budget, :date, :deliverable, :department, :description, :email, :name, :priority, :quantity, :time, :approved, :comments
 
+  validate :date_is_less_than_one_week_from_today
+
+
+  def date_is_less_than_one_week_from_today
+  	if date<(Date.today+7)
+  		errors.add(:date,"-- the Request Date is invalid.")
+    end
+  end
+ 
+
 def self.search(search)
   if search
     where('name LIKE ?', "%#{search}%")
