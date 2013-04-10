b7b5cd3e7ac8a455dfad0657af809d888f2a2f3b
moza <moza@lillypulitzer.com>
2013-04-10 16:46:02 -0400

    Error
    the variable :test was killing rake migration on heroku

--- db/migrate/20130404195823_add_comments_and_approved_to_requests.rb
+++ db/migrate/20130404195823_add_comments_and_approved_to_requests.rb
@@ -1,6 +1,6 @@
 class AddCommentsAndApprovedToRequests < ActiveRecord::Migration
   def change
-    add_column :requests, :comments, :test
+    add_column :requests, :comments
     add_column :requests, :approved, :boolean, default: false
   end
 end
